1b2d6e0afa9cb0aa45a2d52bbfdff8b4799ab36a
edu04 <edu04@EDUPC04.n00d01.kis.co.jp>
2020-04-22 13:20:16 +0900

    first

---
+++ src/lesson/Main.java
@@ -0,0 +1,24 @@
+package lesson;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("さかえこうた");
+		
+		int tuki;
+		tuki = 4;
+		int hi;
+		hi = 22;
+		System.out.println(tuki + "月");
+		System.out.println(hi + "日");
+		System.out.println("今日は" + tuki + "月" + hi + "日です。");
+		
+		double p;
+		p =3.14;
+		System.out.println("円周率は" + p + "です");
+		
+		
+	}
+
+}
