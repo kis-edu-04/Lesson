c96bbdca982158f0ce2fbb812b90cfad56b14707
edu04 <edu04@EDUPC04.n00d01.kis.co.jp>
2020-04-23 11:39:24 +0900

    for演習追記

--- src/lesson/Loop2.java
+++ src/lesson/Loop2.java
@@ -11,13 +11,13 @@ public class Loop2 {
 		}	
 		System.out.println("数字を入力");
 		int num =new java.util.Scanner(System.in).nextInt();
-		 for(int i=1; i<num; i++) {
+		 for(int i=0; i<num; i++) {
 			 System.out.println(i);
 		}
 	    for(int i =2 ; i<=10 ; i=i+2) {
 	    	System.out.println(i);
 	    }
-	    for(int i=1; i<=5 ;i++) {
+	    for(int i=5; i>=1 ;i=i-1) {
 	    	System.out.print(i);
 	    	System.out.print(" ");
 	    }
