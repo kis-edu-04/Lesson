60da0c5fb55880718e7f904f3ed5dd732b0591bd
edu04 <edu04@EDUPC04.n00d01.kis.co.jp>
2020-04-23 16:34:53 +0900

    coin演習

---
+++ src/lesson/Coin.java
@@ -0,0 +1,19 @@
+package lesson;
+
+public class Coin {
+
+	public static void main(String[] args) {
+		
+		
+		int[] coins = {500,100,50,10,5,1};
+		int sum =0;
+		for(int coin : coins) {
+			
+			System.out.println(coin+"円玉は何枚？");
+			int num = new java.util.Scanner(System.in).nextInt();
+			sum=sum + coin*num;
+		}
+		System.out.println(sum+"円");
+	}
+
+}
